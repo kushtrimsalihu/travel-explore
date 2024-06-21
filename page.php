7b377bc773874bb44c5b3c02391fa9b4489d95e3
Zejdi1 <zejdilimani111@gmail.com>
2024-06-21 23:39:22 +0200

    Update footer.twig
    some changes in the footer and page.php

--- page.php
+++ page.php
@@ -4,4 +4,9 @@ use Timber\Timber;
 
 $context = Timber::context();
 $context['flexible_content'] = get_field('flexible_content'); 
+$context['footer'] = [
+    'logo' => get_field('footer_logo', 'option'),
+    'content' => get_field('footer_content', 'option'),
+];
+
 Timber::render('base.twig', $context);
